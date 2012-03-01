8f1837e3ffec23d203e07b5e86f586b0df19d265
Lar Van Der Jagt <dev+supaspoida@hashrocket.com>
2012-03-01 16:33:04 -0500

    Add ui controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ViewResolverExample::Application.routes.draw do
+  match '/ui(/:action)', controller: 'ui'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
