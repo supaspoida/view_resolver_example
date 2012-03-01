f7387398a3db72992a4408e75ffed93f8cf17202
Lar Van Der Jagt <dev+supaspoida@hashrocket.com>
2012-03-01 16:17:12 -0500

    rails new view_resolver_example -O -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ViewResolverExample::Application.config.secret_token = '4e9e165b38b93c4264992e565beea7b4bd44dd74f95100795c9538831fc1615c3a8d639fc4de0def308d29ff590c31a709d766fc520f017468fcc946ec18d1d7'
